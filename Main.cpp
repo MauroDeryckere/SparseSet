a6567afb8f7c39f72c4f57f9feb0aac81fcad6ca
Mauro Deryckere <mauro.deryckere@gmail.com>
2024-06-09 23:21:44 +0200

    Basic SparseSet functionality
    Changed project settings to warnings lvl4 and C++ 20
    Add a templated KeyType for the SparseSet
    
    Contains function
    Emplace function

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,23 @@
+#include "SparseSet.h"
+
+void TestSparseSetInit();
+void TestSparseSetEmplace();
+
 int main() 
 {
+    TestSparseSetInit();
+    TestSparseSetEmplace();
+
     return 0;
+}
+
+void TestSparseSetInit()
+{
+    Internal::SparseSet<int> set{ };
+}
+
+void TestSparseSetEmplace()
+{
+    Internal::SparseSet<int> set{ };
+
 }
--- SparseSet.h
+++ SparseSet.h
@@ -1,1 +1,71 @@
-#pragma once
+#ifndef SPARSE_SET
+#define SPARSE_SET
+
+#include <vector>
+
+#include <cassert>
+#include <string>
+
+#include <type_traits>
+#include <limits>
+
+namespace Internal
+{
+	namespace Impl
+	{
+		template<typename T>
+		concept KeyType = requires
+		{
+			std::is_integral_v<T> && std::is_unsigned_v<T>;
+			std::numeric_limits<T>::max();
+		};
+	}
+
+	template<typename Val, Impl::KeyType KeyType = uint32_t>
+	class SparseSet final
+	{
+	public:
+		SparseSet() = default;
+		SparseSet(KeyType reserveSize) :
+			m_SparseArr(reserveSize, INVALID_INDEX)
+		{ }
+
+		~SparseSet() = default;
+
+		[[nodiscard]] size_t Size() const noexcept { return m_DenseArr.size(); }
+
+		[[nodiscard]] bool Contains(KeyType element) const noexcept { element < m_SparseArr.size() && m_SparseArr[element] != INVALID_INDEX; }
+
+		template<typename... Args>
+		requires std::is_constructible_v<Val, Args...>
+		Val& Emplace(KeyType element, Args&&... args) noexcept
+		{
+			assert(!Contains(element) && "The sparsetalready contains a value for" + std::to_string(element) );
+
+			if (element >= m_SparseArr.size())
+			{
+				m_SparseArr.resize(element + 1, INVALID_INDEX);
+			}
+
+			m_SparseArr[element] = m_DenseArr.size();
+
+			m_DenseArr.emplace_back(element);
+			return m_PackedValArr.emplace_back(std::forward<Args>(args)...);
+		}
+
+		bool Remove(KeyType element) noexcept
+		{
+
+		}
+
+	private:
+		static constexpr KeyType INVALID_INDEX = std::numeric_limits<KeyType>::max();
+
+		std::vector<KeyType> m_SparseArr{ };
+
+		std::vector<KeyType> m_DenseArr{ };
+		std::vector<Val> m_PackedValArr{ };
+	};
+}
+
+#endif
