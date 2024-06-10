8ae06f75f195804b74ad7cb565de25c067ce4f76
Mauro Deryckere <mauro.deryckere@gmail.com>
2024-06-10 17:01:19 +0200

    Remove, try_emplace, iteration
    Added iterator suport
    Changed naming convention to better match with the standard libary
    
    try_emplace function
    remove function
    erase function
    clear function

--- Main.cpp
+++ Main.cpp
@@ -4,28 +4,80 @@
 
 void TestSparseSetInit();
 void TestSparseSetEmplace();
+void TestSparseSetErase();
+void TestSparseSetIteration();
 
 int main() 
 {
     TestSparseSetInit();
     TestSparseSetEmplace();
+    TestSparseSetErase();
+    TestSparseSetIteration();
 
     return 0;
 }
 
 void TestSparseSetInit()
 {
-    Internal::SparseSet<int> set{ 20 };
+    std::cout << "INIT \n";
 
-    std::cout << "Size: " << set.Size() << "\n";
-    std::cout << "Sparse Size: " << set.SparseSize() << "\n";
+    Internal::sparse_set<int> set{ 20 };
+
+    std::cout << "Size: " << set.size() << "\n";
+    std::cout << "Sparse Size: " << set.sparse_size() << "\n";
 }
 
 void TestSparseSetEmplace()
 {
-    Internal::SparseSet<int> set{ };
-    set.Emplace(2, 0);
+    std::cout << "\nEMPLACE \n";
+
+    Internal::sparse_set<int> set{ };
+    auto& element = set.emplace(2, 99);
+    std::cout << "element: " << element << "\n";
     //set.Emplace(2, 0); //assert triggered as expected
 
-    std::cout << std::boolalpha << set.Contains(2) << "\n";
+    std::cout << std::boolalpha << set.contains(2) << "\n";
+
+    //try emplacing the same element using the try emplace
+    std::cout << std::boolalpha << set.try_emplace(2, 99).second << "\n";
+    std::cout << std::dec << (*set.try_emplace(2, 99).first) << "\n";
+}
+
+void TestSparseSetErase()
+{
+    std::cout << "\nERASE \n";
+
+    Internal::sparse_set<int> set{ };
+    set.emplace(0, 100);
+    set.emplace(1, 200);
+    set.emplace(2, 300);
+
+    std::cout << std::boolalpha << set.contains(0) << "\n";
+    std::cout << std::boolalpha << set.contains(1) << "\n";
+    std::cout << std::boolalpha << set.contains(2) << "\n";
+    std::cout << "\n";
+
+    set.erase(1);
+    std::cout << std::boolalpha << set.contains(0) << "\n";
+    std::cout << std::boolalpha << set.contains(1) << "\n";
+    std::cout << std::boolalpha << set.contains(2) << "\n";
+    std::cout << "\n";
+
+    std::cout << std::boolalpha << set.remove(1) << "\n";
+}
+
+void TestSparseSetIteration()
+{
+    std::cout << "\nITERATION \n";
+
+    Internal::sparse_set<int> set{ };
+    set.emplace(0, 0);
+    set.emplace(1, 1);
+    set.emplace(2, 2);
+    set.emplace(3, 3);
+
+    for (auto&& e : set)
+    {
+        std::cout << e << "\n";
+    }
 }
--- SparseSet.h
+++ SparseSet.h
@@ -3,45 +3,77 @@
 
 #include <vector>
 
-#include <cassert>
-#include <string>
-
 #include <type_traits>
 #include <limits>
+#include <utility>
+
+#include "InternalAssert.h"
 
 namespace Internal
 {
 	namespace Impl
 	{
 		template<typename T>
-		concept KeyType = requires
-		{
-			std::is_integral_v<T> && std::is_unsigned_v<T>;
-			std::numeric_limits<T>::max();
-		};
+		concept KeyType = std::is_integral_v<T> && std::is_unsigned_v<T> 
+						&& std::is_trivially_copyable_v<T> && std::is_swappable_v<T>; //erase function
+
+		template<typename T>
+		concept MoveAssignmentVal = std::is_move_assignable_v<T>;
+
+		//We only want to move construct when we arent able to move assign
+		template<typename T>
+		concept MoveConstructVal = !MoveAssignmentVal<T> && std::is_move_constructible_v<T>;
+
+		template<typename T>
+		concept ValType = std::is_nothrow_destructible_v<T> 
+						&& (MoveAssignmentVal<T> || MoveConstructVal<T>);
 	}
 
-	template<typename Val, Impl::KeyType KeyType = uint32_t>
-	class SparseSet final
+	template<Impl::ValType Val, Impl::KeyType KeyType = uint32_t>
+	class sparse_set final
 	{
 	public:
-		SparseSet() = default;
-		SparseSet(KeyType reserveSize) :
-			m_SparseArr(reserveSize, INVALID_INDEX)
+		sparse_set() = default;
+		sparse_set(KeyType sparseSize) :
+			m_SparseArr(sparseSize, INVALID_INDEX)
 		{ }
 
-		~SparseSet() = default;
+		~sparse_set() = default;
 
-		[[nodiscard]] size_t Size() const noexcept { return m_DenseArr.size(); }
-		[[nodiscard]] size_t SparseSize() const noexcept { return m_SparseArr.size(); }
+		using Iterator = typename std::vector<Val>::iterator;
+		using ConstIterator = typename std::vector<Val>::const_iterator;
+		
+		using ReverseIterator = typename std::vector<Val>::reverse_iterator;
+		using ConstReverseIterator = typename std::vector<Val>::const_reverse_iterator;
 
-		[[nodiscard]] bool Contains(KeyType element) const noexcept { return element < m_SparseArr.size() && m_SparseArr[element] != INVALID_INDEX; }
+		Iterator begin() noexcept { return m_PackedValArr.begin(); }
+		Iterator end() noexcept { return m_PackedValArr.end(); }
+		ConstIterator begin() const noexcept { return m_PackedValArr.begin(); }
+		ConstIterator end() const noexcept { return m_PackedValArr.end(); }
 
+		ReverseIterator rbegin() noexcept { return m_PackedValArr.rbegin(); }
+		ReverseIterator rend() noexcept { return m_PackedValArr.rend(); }
+		ConstReverseIterator rbegin() const noexcept { return m_PackedValArr.rbegin(); }
+		ConstReverseIterator rend() const noexcept { return m_PackedValArr.rend(); }
+
+		[[nodiscard]] size_t size() const noexcept { return m_DenseArr.size(); }
+		[[nodiscard]] size_t sparse_size() const noexcept { return m_SparseArr.size(); }
+
+		void clear() noexcept
+		{
+			m_DenseArr.clear();
+			m_PackedValArr.clear();
+			m_SparseArr.clear();
+		}
+
+		[[nodiscard]] bool contains(KeyType element) const noexcept { return element < m_SparseArr.size() && m_SparseArr[element] != INVALID_INDEX; }
+
+		//Do not emplace the same element in the set twice, use try_emplace if this is a concern.
 		template<typename... Args>
 		requires std::is_constructible_v<Val, Args...>
-		Val& Emplace(KeyType element, Args&&... args) noexcept
+		Val& emplace(KeyType element, Args&&... args) noexcept
 		{
-			assert(!Contains(element));
+			ASSERT(!contains(element), "Element already in set!");
 
 			if (element >= m_SparseArr.size())
 			{
@@ -54,10 +86,49 @@ namespace Internal
 			return m_PackedValArr.emplace_back(std::forward<Args>(args)...);
 		}
 
-		//bool Remove(KeyType element) noexcept
-		//{
+		template<typename... Args>
+		requires std::is_constructible_v<Val, Args...>
+		std::pair<Iterator, bool> try_emplace(KeyType element, Args&&... args) noexcept
+		{
+			if (contains(element))
+			{
+				return { m_PackedValArr.begin() + m_SparseArr[element], false};
+			}
+			
+			emplace(element, std::forward<Args>(args)...);
+			return { Iterator{ m_PackedValArr.end() - 1 }, true };
+		}
+		
+		//Do not erase an element that does not exist, use remove instead if this is a concern.
+		void erase(KeyType element) noexcept
+		{
+			ASSERT(contains(element), "Element not in set!");
 
-		//}
+			//const auto last{ m_DenseArr.back() };
+
+			if constexpr(Impl::MoveAssignmentVal<Val>)
+			{
+				m_PackedValArr[m_SparseArr[element]] = std::move(m_PackedValArr.back());
+			}
+			else if constexpr(Impl::MoveConstructVal<Val>)
+			{
+				m_PackedValArr[m_SparseArr[element]].~Val();
+				new (&m_PackedValArr[m_SparseArr[element]]) Val(std::move(m_PackedValArr.back()));
+			}
+
+			m_DenseArr[m_SparseArr[element]] = m_DenseArr.back();
+
+			//std::swap(m_SparseArr[last], m_SparseArr[element]);
+			m_SparseArr[element] = INVALID_INDEX;
+			
+			m_DenseArr.pop_back();
+			m_PackedValArr.pop_back();
+		}
+
+		bool remove(KeyType element) noexcept
+		{
+			return contains(element) && (erase(element), true);
+		}
 
 	private:
 		static constexpr KeyType INVALID_INDEX = std::numeric_limits<KeyType>::max();
