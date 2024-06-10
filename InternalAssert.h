0becf62ccd4baecc666f378eac09cc5d82046de6
Mauro Deryckere <mauro.deryckere@gmail.com>
2024-06-10 15:26:51 +0200

    Added assert macro

---
+++ InternalAssert.h
@@ -0,0 +1,31 @@
+#ifndef ENG_ASSERT
+#define ENG_ASSERT
+
+#include <iostream>
+
+namespace Internal
+{
+#ifndef NDEBUG
+    #define DEBUG_ASSERT(condition, message) \
+        do { \
+            if (!(condition)) { \
+                std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
+                          << " line " << __LINE__ << ": " << message << std::endl; \
+                std::abort(); \
+            } \
+        } while (false)
+#else
+    #define DEBUG_ASSERT(condition, message) ((void)0)
+#endif
+
+#define ASSERT(condition, message) \
+    do { \
+        if (!(condition)) { \
+            std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
+                        << " line " << __LINE__ << ": " << message << std::endl; \
+            std::abort(); \
+        } \
+    } while (false)
+}
+
+#endif
